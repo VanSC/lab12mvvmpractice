857c492e8c4fc15a880ecde70d49862e55575db1
Alumno <Alumno@LAB1509-09>
2023-12-01 16:15:22 -0500

    Agregar archivos de proyecto.

---
+++ lab12mvvmpractice/lab12mvvmpractice/ViewModels/FormularioViewModel.cs
@@ -0,0 +1,59 @@
+﻿using lab12mvvmpractice.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace lab12mvvmpractice.ViewModels
+{
+    internal class FormularioViewModel : ViewModelBase
+    {
+        //propiedades
+        string titulo;
+        public string Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                if (titulo != value)
+                {
+                    titulo = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        string descripcion;
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set
+            {
+                if (descripcion != value)
+                {
+                    descripcion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        string status;
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                if (status != value)
+                {
+                    status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        //comandos
+        public ICommand Guardar { protected set; get; }
+
+        public ICommand Listar { protected set; get; }
+    }
+}
---
+++ lab12mvvmpractice/lab12mvvmpractice/Views/Formulario.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab12mvvmpractice.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Formulario : ContentPage
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.FormularioViewModel();
+        }
+    }
+}
