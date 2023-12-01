310447c2b35724242ffb183e2c7caebbf2d6e7f8
VanSC <vanessa.choque@tecsup.edu.pe>
2023-12-01 17:16:04 -0500

    Añadir cambios en vista

---
+++ lab12mvvmpractice/lab12mvvmpractice/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab12mvvmpractice.Models
+{
+    public class TaskModel
+    {
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string status { get; set;}
+
+        public TaskModel() { }
+    }
+}
--- lab12mvvmpractice/lab12mvvmpractice/ViewModels/FormularioViewModel.cs
+++ lab12mvvmpractice/lab12mvvmpractice/ViewModels/FormularioViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using lab12mvvmpractice.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
+using lab12mvvmpractice.Models;
+using Xamarin.Forms;
 
 namespace lab12mvvmpractice.ViewModels
 {
@@ -55,5 +58,44 @@ namespace lab12mvvmpractice.ViewModels
         public ICommand Guardar { protected set; get; }
 
         public ICommand Listar { protected set; get; }
+
+        //public List<string> MuchosDatos { protected set; get; }
+        //los datos calculado no se agregan en el modelo pero si en el viewmodel
+        //propiedad de listas
+
+        ObservableCollection<TaskModel> task;
+        public ObservableCollection<TaskModel> Tasks
+        {
+            get { return task; }
+            set
+            {
+                if (task != value)
+                {
+                    task = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private List<TaskModel> TaskModels;
+        public FormularioViewModel()
+        {
+            Tasks = new ObservableCollection<TaskModel>();
+            TaskModels = new List<TaskModel>();
+
+            Guardar = new Command(() =>
+            {
+                TaskModel task = new TaskModel();
+                task.titulo = this.Titulo;
+                task.descripcion = this.Descripcion;
+                task.status = this.Status;
+                TaskModels.Add(task);
+            });
+
+            Listar = new Command(() =>
+            {
+                Tasks = new ObservableCollection<TaskModel>(TaskModels);
+            });
+        }
     }
 }
